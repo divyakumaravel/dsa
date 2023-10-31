da6476b34d935a4e463163bb977af2e87a545bf3
Divya K <divya.k@thoughtworks.com>
2023-10-31 11:56:32 +0530

    Solved. DSA Array - Two sum

---
+++ src/test/java/com/example/practice/problems/arrays/TwoSumTest.java
@@ -0,0 +1,31 @@
+package com.example.practice.problems.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    TwoSum twoSum = new TwoSum();
+
+    @Test
+    void shouldReturnArray() {
+        int[] result = twoSum.twoSum(new int[]{2, 7, 11, 15}, 9);
+
+        assertArrayEquals(new int[]{0, 1}, result);
+    }
+
+    @Test
+    void shouldReturnArray1() {
+        int[] result = twoSum.twoSum(new int[]{3, 2, 4}, 6);
+
+        assertArrayEquals(new int[]{1, 2}, result);
+    }
+
+    @Test
+    void shouldReturnArray2() {
+        int[] result = twoSum.twoSum(new int[]{3, 3}, 6);
+
+        assertArrayEquals(new int[]{0, 1}, result);
+    }
+}
