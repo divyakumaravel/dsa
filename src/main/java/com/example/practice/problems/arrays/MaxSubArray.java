2011f8881dc60391a3e3b973a21973ce0f7400ca
divyakumaravel <divyakk98@gmail.com>
2024-04-30 10:55:12 +0530

    Arrays. Max sub array

---
+++ src/main/java/com/example/practice/problems/arrays/MaxSubArray.java
@@ -0,0 +1,26 @@
+package com.example.practice.problems.arrays;
+
+public class MaxSubArray {
+
+    public static void main(String[] args) {
+        int max = maxSubArray(new int[]{-2, 1});
+        System.out.println("Max " + max);
+    }
+
+    private static int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (sum >= 0) {
+                sum += nums[i];
+            } else {
+                sum = nums[i];
+            }
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        return max;
+    }
+}
